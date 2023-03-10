64eda0ccb03bf2734186819993f94fde9dbfc697
Martin Christoph Frank <martin.frank@hs-augsburg.de>
2023-03-10 09:04:13 +0100

    all conversation delete button works now

--- app.py
+++ app.py
@@ -7,24 +7,27 @@ from chat_handler import get_answer
 app = Flask(__name__)
 
 db = TinyDB('db.json')
-conversations = db.table('conversations')
 
 @app.route('/')
 def index():
-    conversations_list = conversations.all()
+    conversations_list = db.table('conversations').all()
     return render_template('index.html', conversations=[conv["conversation_id"] for conv in conversations_list])
 
 @app.route('/send_message', methods=["POST"])
 def send_message():
-    message = request.form.get('message', '').strip()
-    conversation_id = request.form.get('conversation_id', '').strip()
+    message = request.json['message']
+    conversation_id = request.json['conversation_id']
+
+
+    conversations = db.table('conversations')
     conversation = conversations.get(Query().conversation_id == conversation_id)
+
 
     messages = conversation.get('messages', [{"role": "system", "content": "You are a helpful assistant."}])
     messages.append({"role": "user", "content": message})
     new_response = get_answer(messages)
     messages.append(new_response)
-    conversations.update({'messages': messages}, Query().conversation_id == conversation_id)
+    db.table('conversations').update({'messages': messages}, Query().conversation_id == conversation_id)
     return jsonify({'conversation_id': conversation_id, "title": conversation.get("title", "New Conversation")})
 
 @app.route("/make_new_conversation")
@@ -35,21 +38,30 @@ def make_new_conversation():
         "title": "New Conversation",
         'messages': []
         }
-    conversations.insert(conversation)
+    db.table('conversations').insert(conversation)
     return jsonify({key: conversation[key] for key in ["conversation_id", "title"]})
+
+@app.route('/delete_all_conversations', methods=["POST"])
+def delete_all_conversations():
+    db.drop_table('conversations')
+    return '', 204
 
 @app.route('/get_messages')
 def get_messages():
     conversation_id = request.args.get('conversation_id', '').strip()
     if conversation_id:
-        conversation = conversations.get(Query().conversation_id == conversation_id)
+        conversation = db.table('conversations').get(Query().conversation_id == conversation_id)
         return jsonify(conversation)
     else:
         return jsonify({})
 
 @app.route('/get_conversations')
 def get_conversations():
-    conversations_list = [{"conversation_id": conv["conversation_id"], "created": conv["created"], "title": conv["title"]} for conv in conversations.all()]
+    convs = db.table('conversations').all()
+    print(convs)
+    if not convs:
+        return jsonify([])
+    conversations_list = [{"conversation_id": conv["conversation_id"], "created": conv["created"], "title": conv["title"]} for conv in convs]
     conversations_list.sort(key=lambda x: x["created"])
     return jsonify(conversations_list)
 
